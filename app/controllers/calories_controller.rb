e68cb0f01e6cd6f55a5cc969dbe74632a9dd60af
Ruben <ssrcruz@gmail.com>
2015-01-29 17:31:06 -0500

    Add edit and _index.

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,4 +1,5 @@
 class CaloriesController < ApplicationController
+  before_action :set_calorie, only: [:show, :edit, :update, :destroy]
 
   def calorie_params
     params.require(:calorie).permit(:logged_on, :calories_consumed)
@@ -16,7 +17,45 @@ class CaloriesController < ApplicationController
     @calorie = Calorie.new(calorie_params)
     respond_to do |format|
       if @calorie.save
-        format.html { redirect_to @calorie, notice: "Calories were enetered successfully." }
-        format.json { render :}
+        format.html { redirect_to @calorie, notice: "Calories were entered successfully." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @calorie.update(calories_params)
+        format.html { redirect_to @calorie, notice: 'Calories logged successfully.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @calorie.update(calorie_params)
+        format.html { redirect_to @calorie, notice: 'Calories logged successfully.'}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @calorie.destroy
+    respond_to do |format|
+      format.html { redirect_to calories_url, notice: 'Calories logged successfully.'}
+    end
+  end
+
+  private def set_calorie
+      @calorie = Calorie.find(params[:id])
+  end
+
+  private def calorie_params
+      params.require(:calorie).permit(:logged_on, :steps_taken)
   end
 end
