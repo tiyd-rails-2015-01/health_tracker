d31a47536594a86206ae74458ebf5172a4cde211
Thomas Midgett <tmidge20@gmail.com>
2015-01-30 12:14:22 -0500

    fixes time bug? #shipit?

--- app/models/calorie.rb
+++ app/models/calorie.rb
@@ -3,7 +3,7 @@ class Calorie < ActiveRecord::Base
 
       total = 0
       calorie_array = []
-      Calorie.where('entered_on BETWEEN ? AND ?', (DateTime.now.beginning_of_day-1), (DateTime.now.end_of_day-1)).all.each do |calorie|
+      Calorie.where('entered_on BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all.each do |calorie|
         calorie_array << calorie.calorie
       end
       total = calorie_array.reduce(:+)
