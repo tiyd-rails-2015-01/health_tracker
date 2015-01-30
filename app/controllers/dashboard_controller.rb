c0078007c34e9e8e110e7e691c8a8a0703b98eb1
Ashley Ellis <anellis12@gmail.com>
2015-01-30 00:27:50 -0500

    Add styling to dashboard page, set dashboard as root, add navigation. #shipit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def status
+    @total_exercises= Exercise.count
+    @total_weight_lost= Weight.count
+    @total_steps= Step.count
+    @total_calories_consumed= ConsumedCalorie.count
   end
 end
