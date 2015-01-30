8304b683211d0731b94e0fcae6aa865e3e05e3bf
Thomas Midgett <tmidge20@gmail.com>
2015-01-30 14:11:46 -0500

    Subtracts exercise calories from total on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
   def home
     @c_total= Calorie.c_total
+    @e_total= Exercise.e_total
+    @f_total= (Calorie.c_total.blank?) ? "Always eat before attempting exercise." : (Calorie.c_total-Exercise.e_total)
   end
 end
+
+#if Calorie.c_total.blank?
+#            @f_total = (0-@e_total)
+#          elsif Exercise.e_total.blank?
+#            @f_total = @c_total
+#          else
+#            @f_total = Calorie.c_total-Exercise.e_total
+#          end
+#
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,20 @@
 class Exercise < ActiveRecord::Base
   #"Cardio","Strength Training","Spin Class","Jazzercise","Kick-boxing"
-  
+  def self.e_total
+      total = 0
+      Exercise.where('entered_on BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all.each do |exercise|
+        if exercise.exercise == "Cardio"
+          total += 150
+        elsif exercise.exercise == "Strength Training"
+          total += 250
+        elsif exercise.exercise == "Spin Class"
+          total += 200
+        elsif exercise.exercise == "Jazzercise"
+          total += 500
+        elsif exercise.exercise == "Kick-boxing"
+          total += 100
+        end
+      end
+      return total
+  end
 end
