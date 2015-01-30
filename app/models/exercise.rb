2038aca9084355a246f8baf0f2fdd9aaee5ba6e3
Drew Smith <greenwichdrew@gmail.com>
2015-01-30 14:26:16 -0500

    new commit

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,22 +1,29 @@
 class Exercise < ActiveRecord::Base
 
   def self.exercise_count(exercise)
-    total = 0
-    Exercise.all.each do |exercise|
-      if exercise == exercise.exercise_type
-        total += exercise.exercise_amount.to_f
+    total = 0.0
+    Exercise.all.each do |e|
+      if exercise == e.exercise_type
+        total += e.exercise_amount.to_f
       end
     end
     return total
   end
 
 
-    def self.total_calories
-      total = 0
-      Exercise.all.each do |exercise|
-        total += exercise.calories_consumed.to_f
-      end
-      return total
+  def self.total_calories
+    total = 0
+    Exercise.all.each do |exercise|
+      total += exercise.calories_consumed.to_f
     end
+    return total
+  end
+
+  # def calculate_calories
+  #   exercise_type
+  #   exercise_amount
+  #   result =
+  #   return result
+  # end
 
 end
