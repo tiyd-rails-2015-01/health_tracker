28f8ecf1a170d5e3bebe35252080fe1b41ae778e
Topstitch <kallen@spoonflower.com>
2015-01-30 18:09:40 -0500

    Trying to fix route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   resources :exercises
 
-  get 'dashboard/index'
+  #get 'dashboard/index'
+  root '../dashboard/index.html?somenumber=<%=@total_amount%>'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
