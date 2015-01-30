bba7207a318698829fb2bd93e105a92fb8ccd644
Ashley Ellis <anellis12@gmail.com>
2015-01-30 01:41:46 -0500

    Still sorting th graph, commenting out for now to work on later. #shipit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,11 @@ class DashboardController < ApplicationController
     @total_steps= Step.sum(:steps_taken)
     @total_calories_consumed= ConsumedCalorie.sum(:calorie_amount)
 
-    # graph={@total_exercises, @total_weight_lost, @total_steps, @total_calories_consumed}
-    # @graph=graph.sort
-    # @graph[0]="height: 95%;"
+    graph=[@total_exercises, @total_weight_lost, @total_steps, @total_calories_consumed]
+    @graph=graph.sort.reverse!
+
+    @graph_hash={@graph[0]=>"height: 95%;", "Weight lost"=>@total_weight_lost, "Steps"=>@total_steps, "Calories Consumed"=>@total_calories_consumed}
+
   end
+
 end
