f388667a27aa78ce802f9c8d07bd26b1de85166d
Rocky Randalls <rrandalls@gmail.com>
2015-01-30 17:43:45 -0500

    Fix Weight default value to 0

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -3,6 +3,10 @@ class Weight < ActiveRecord::Base
   validates :daily_weight, length: {maximum: 5}
 
   def self.todays_weight
-    Weight.where(recorded_on: Date.today).first.daily_weight ||= 0.0
+    if Weight.where(recorded_on: Date.today).first
+      Weight.where(recorded_on: Date.today).first.daily_weight
+    else
+      0.0
+    end
   end
 end
