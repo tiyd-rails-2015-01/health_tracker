e13a7d9353d5d842e47def85b694e31a796043d9
Rocky Randalls <rrandalls@gmail.com>
2015-01-30 14:23:11 -0500

    Add CalorieConsumption#calorie_total

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+
+  def index
+    @calorie_count = CalorieConsumption.calorie_total
+  end
 end
--- app/models/calorie_consumption.rb
+++ app/models/calorie_consumption.rb
@@ -1,6 +1,12 @@
 class CalorieConsumption < ActiveRecord::Base
-
   def self.calorie_total
-    sum(:calorie_intake)
+    sum = 0
+    CalorieConsumption.all.each do |s|
+      if s.consumed_on >= DateTime.now.beginning_of_day &&
+         s.consumed_on <= DateTime.now.end_of_day
+        sum += s.calorie_intake
+      end
+    end
+    sum
   end
 end
