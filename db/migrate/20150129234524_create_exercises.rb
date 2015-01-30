5ffaf15288dd6a8962a23bce434a036c209b152c
Rocky Randalls <rrandalls@gmail.com>
2015-01-30 09:19:18 -0500

    Fix migration file Exercise

--- db/migrate/20150129234524_create_exercises.rb
+++ db/migrate/20150129234524_create_exercises.rb
@@ -3,7 +3,7 @@ class CreateExercises < ActiveRecord::Migration
     create_table :exercises do |t|
       t.datetime :performed_on
       t.string :exercise_type
-      t.decimals :exercise_duration
+      t.decimal :exercise_duration
       t.integer :calorie_burn
 
       t.timestamps null: false
