741547c34294fefa1635c669db81f2d202a81a93
Ashley Ellis <anellis12@gmail.com>
2015-01-30 08:20:58 -0500

    Bar graph working! #shipit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,9 +6,13 @@ class DashboardController < ApplicationController
     @total_calories_consumed= ConsumedCalorie.sum(:calorie_amount)
 
     graph=[@total_exercises, @total_weight_lost, @total_steps, @total_calories_consumed]
-    @graph=graph.sort.reverse!
+    @total=graph.reduce(:+)
 
-    @graph_hash={@graph[0]=>"height: 95%;", "Weight lost"=>@total_weight_lost, "Steps"=>@total_steps, "Calories Consumed"=>@total_calories_consumed}
+    @exercises_percent=(@total_exercises/@total) * 100
+    @weight_lost_percent=(@total_weight_lost/@total) * 100
+    @steps_percent=(@total_steps/@total) * 100
+    @calories_percent=(@total_calories_consumed/@total) * 100
+    # @graph_hash={@graph[0]=>"height: 95%;", "Weight lost"=>@total_weight_lost, "Steps"=>@total_steps, "Calories Consumed"=>@total_calories_consumed}
 
   end
 
