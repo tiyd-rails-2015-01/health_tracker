314732b3d59fff209d0247cbc28007228841411e
Maria <mliberov@gmail.com>
2015-01-31 09:08:23 -0500

    Make changes to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150130213044) do
+
   create_table "calories", force: :cascade do |t|
     t.date     "date"
     t.string   "meal"
     t.integer  "calories_consumed"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.date     "date"
