89b5450fdcd751db5ea3a89f99142373bc133eb3
Maria <mliberov@gmail.com>
2015-01-31 18:58:43 -0500

    Resolve merge conflict #shipit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def home
+<<<<<<< HEAD
     @steps_total 
+=======
+    @steps_total = "banana"
+>>>>>>> 575d2a8... Fix meal dropdown menu in calories
 
   end
 end
