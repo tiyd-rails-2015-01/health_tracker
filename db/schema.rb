4e11281c88afcfb55ce72584d1627af9e87b1662
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-31 11:12:35 -0500

    Add Home page with links between all pages #shipit

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150130213044) do
+
   create_table "calories", force: :cascade do |t|
     t.date     "date"
     t.string   "meal"
