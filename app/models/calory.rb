4c188a963b365823a3af43716300bbe44b03ee19
Drew Smith <greenwichdrew@gmail.com>
2015-01-31 16:00:30 -0500

    fix welcome page

--- app/models/calory.rb
+++ app/models/calory.rb
@@ -5,7 +5,7 @@ class Calory < ActiveRecord::Base
   def self.total
     total = 0
     Calory.all.each do |calories|
-      total += calories.calories_consumed
+      total += calories.calories_consumed.to_f
     end
     return total
   end
