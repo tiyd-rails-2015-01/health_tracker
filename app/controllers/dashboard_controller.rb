f0a40db37be0e87064024c214d02d87b39a41762
Ashley Ellis <anellis12@gmail.com>
2015-01-31 19:15:29 -0500

    Edit dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,16 +13,17 @@ class DashboardController < ApplicationController
       @total_weight_lost= Weight.first.user_weight - Weight.last.user_weight
       graph=[@total_exercises, @total_weight_lost, @total_steps, @total_calories_consumed]
       @total=graph.reduce(:+)
+      @exercises_percent=(@total_exercises/@total) * 100
+      @weight_lost_percent=(@total_weight_lost/@total) * 100
+      @steps_percent=(@total_steps/@total) * 100
+      @calories_percent=(@total_calories_consumed/@total) * 100
     end
 
 
     if @num_weights> 0 && @total_exercises > 0
       @net_calories = Joule.calories_entered_today - Exercise.calories_burned.to_f
     end
-    @exercises_percent=(@total_exercises/@total) * 100
-    @weight_lost_percent=(@total_weight_lost/@total) * 100
-    @steps_percent=(@total_steps/@total) * 100
-    @calories_percent=(@total_calories_consumed/@total) * 100
+
 
   end
 
