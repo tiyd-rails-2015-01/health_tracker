7e45da2f0019d93e087323fb8c6744c329034042
Ashley Ellis <anellis12@gmail.com>
2015-01-31 19:08:58 -0500

    Move total weight lost calculation into an if so that it does not try to run if no weights have been entered

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
   def status
     @total_exercises= Exercise.sum(:duration)
     @entered_weight= Weight.sum(:user_weight)
-    @total_weight_lost= Weight.first.user_weight - Weight.last.user_weight
     @total_steps= Step.sum(:steps_taken)
     @total_calories_consumed= Joule.sum(:consumed_joules)
     @num_weights =Weight.count
@@ -12,6 +11,7 @@ class DashboardController < ApplicationController
 
     if @entered_weight > 0
       @latest_weight= Weight.last.user_weight
+      @total_weight_lost= Weight.first.user_weight - Weight.last.user_weight
     end
 
 
